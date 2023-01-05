0f712d6ce13a564b8efc66381047849a635417d2
febathomas123 <97583748+febathomas123@users.noreply.github.com>
2023-01-05 11:52:59 +0530

    Add files via upload

---
+++ matrixopr.py
@@ -0,0 +1,24 @@
+import numpy as np
+x=np.array([[1,2,3],[4,5,6],[7,8,9]])
+y=np.array([[6,7,5],[2,5,8],[3,4,9]])
+z=np.array([[2,5,6],[3,9,2],[2,6,4]])
+print("x^2")
+print(np.multiply(x,x))
+t=((np.multiply(x,x)))
+print("2y")
+v=print(np.multiply(2,y))
+v=(np.multiply(2,y))
+print("x^2+2y")
+print(np.add(t,v))
+r=("x^2+2y")
+print("z^2")
+print(np.multiply(z,z,))
+m=((np.multiply(z,z,)))
+print("z^3")
+print(np.multiply(m,z))
+n=(np.multiply(m,z))
+print("final result is:")
+print(t+v-n)
+
+
+
